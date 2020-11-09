dc44c9a4bf7dd63593ee3bee5cea8435f33a005b
Bianca SchoenPhelan <bianca.schoenphelan@soc-mbp13-bsp.lan>
2020-11-09 09:55:20 +0000

    Lab Test 1: A Possible Coding Solution

---
+++ lab_test1_sol.py
@@ -0,0 +1,34 @@
+# solution to lab test 1
+# OOP Python
+# 2020-21
+
+import collections
+
+class FindUncommonWord:
+    def __init__(self, s1, s2):
+        self.find_un_common(s1, s2)
+
+
+    def find_un_common(self, sen1, sen2):
+        a_split = sen1.lower().split()
+        b_split = sen2.lower().split()
+
+        uncommon_words = set(a_split).symmetric_difference(set(b_split))
+
+        a_counter = collections.Counter(a_split)
+        b_counter = collections.Counter(b_split)
+
+
+        # solution = []
+        # for word in uncommon_words:
+        #     if a_counter[word]==1 or b_counter[word]==1:
+        #         solution.append(word)
+
+        solution = [word for word in uncommon_words if a_counter[word] == 1 or b_counter[word] == 1]
+        print(solution)
+
+
+sentence1 = "You used to love cake but now you don't."
+sentence2 = "We used to enjoy cake but now we don't."
+f = FindUncommonWord(sentence1, sentence2)
+
